762f04825ab49131cb22dbdd2b7311100ac82a68
Jose Javier Vegas <65979544+vegasjj@users.noreply.github.com>
2025-12-02 15:17:27 +0000

    Add Azure deployment configuration with GitGub Actions and Terraform setup for static site

---
+++ azure/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = "1.14.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.52.0"
+    }
+  }
+
+    cloud {}
+}
---
+++ azure/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  default     = "rg-crc-prod-001"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region to deploy resources"
+  default     = "eastus"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the Storage Account"
+  default     = "stcrc002"
+  type        = string
+}
+
+variable "cdn_profile_name" {
+  description = "Name of the CDN Profile"
+  default      = "cdn-crc-prod-001"
+  type        = string
+}
+
+variable "cdn_endpoint_name" {
+  description = "Name of the CDN Endpoint"
+  default     = "ep-crc-prod-001"
+  type        = string
+}
+
+variable "custom_domain_name" {
+  description = "Custom domain for CDN endpoint"
+  default     = "resume.technicalmind.cloud"
+  type        = string
+}
