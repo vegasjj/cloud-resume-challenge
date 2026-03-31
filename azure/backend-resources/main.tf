0a64ddee2eb30489d66149e1e259014733a93aa7
Jose Javier Vegas <65979544+vegasjj@users.noreply.github.com>
2026-03-31 21:22:11 +0000

    Test validation 5

--- azure/backend-resources/main.tf
+++ azure/backend-resources/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_cosmosdb_account" "db" {
   location                        = azurerm_resource_group.rg.location
   resource_group_name             = azurerm_resource_group.rg.name
   offer_type                      = "Standard"
-  local_authentication_disabled   = false
+  local_authentication_disabled   = true
 
   tags = {
     defaultExperience = "Azure Table"
