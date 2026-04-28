218290cb7882de0adf3376e9b0f3c72eda764e55
Jose Javier Vegas <65979544+vegasjj@users.noreply.github.com>
2026-04-28 03:07:50 +0000

    Enhance error handling in visitor counter API with improved logging and user-friendly messages

--- azure/backend-resources/visitor-counter/function_app.py
+++ azure/backend-resources/visitor-counter/function_app.py
@@ -13,11 +13,12 @@ row_key = os.environ['COSMOS_DB_ROW_KEY']
 
 credential = DefaultAzureCredential()
 account_url = f"https://{account_name}.table.cosmos.azure.com:443"
+generic_client_message = "An internal server error occurred, check the logs or contact your administrator"
 
-def create_error_response(message, status):
-    logging.exception(f"An error with status code [{status}] occurred")
+def create_error_response(client_message: str, server_message: str, status: int, exc_info: bool = True):
+    logging.error(f"Status code error [{status}] occurred with message: {server_message}", exc_info=exc_info)
     return func.HttpResponse(
-        body=json.dumps({"message": message}),
+        body=json.dumps({"message": client_message}),
         status_code=status,
         mimetype="application/json"
     )
@@ -46,9 +47,9 @@ def visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
         counter_entity = table_client.get_entity(partition_key=partition_key, row_key=row_key)
         current_value = counter_entity['visitor_counter']
     except ResourceNotFoundError:
-        return create_error_response("The key you want to retrieve doesn't exist", 404)
+        return create_error_response(generic_client_message, "Either the table, partition key or row key doesn't exist or is misspelled", 404)
     except Exception:
-        return create_error_response("The visitor counter couldn't be retrieved: check the logs or contact your administrator", 500)
+        return create_error_response(generic_client_message, "Check the traceback for details", 500)
 
     updated_value = current_value + 1
     counter_entity['visitor_counter'] = updated_value
@@ -56,7 +57,7 @@ def visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
     try:
         table_client.update_entity(mode=UpdateMode.REPLACE, entity=counter_entity)
     except Exception:
-        return create_error_response("Failed to update counter", 500)
+        return create_error_response(generic_client_message, "Failed to update counter", 500)
     return func.HttpResponse(
         json.dumps({"visitor_counter": updated_value}),
         status_code=200,
