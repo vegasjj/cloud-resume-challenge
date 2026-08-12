e23c84cb661f145e2f926e0d16d2eac40a73e130
Jose Javier Vegas <65979544+vegasjj@users.noreply.github.com>
2026-08-12 16:18:24 +0000

    Update Terraform configuration and dependencies to version 5.0.1

--- azure/frontend-resources/terraform.tf
+++ azure/frontend-resources/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~>1.14.5"
+  required_version = "~>1.15.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.57.0"
+      version = "~>5.0.0"
     }
   }
   
